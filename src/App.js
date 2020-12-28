257d7ae35eb33d42db1bf2cef468fccc784efd74
Keenan (עזרא) <reaper31292@gmail.com>
2020-12-28 12:03:25 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+
+    return (
+        <div className="App">
+            <h1>Hello To Do List</h1>
+        </div>
+    )
+};
+
+export default App;
